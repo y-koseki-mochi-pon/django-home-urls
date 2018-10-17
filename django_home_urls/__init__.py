ef42ad08101f83c2aff941e00abd50e60c57ac51
小関 優司 <pxquuqjm0k62new7q4@gmail.com>
2018-10-17 23:21:26 +0900

    add path name (default home)

--- django_home_urls/__init__.py
+++ django_home_urls/__init__.py
@@ -1,6 +1,6 @@
 name = "django_home_urls"
 
-def home(view_name):
+def home(view_name, name="home"):
     from django.urls import path
 
     def f(request, *args, **kwargs):
@@ -8,4 +8,4 @@ def home(view_name):
         path = reverse(view_name)
         func = resolve(path).func
         return func(request, *args, **kwargs)
-    return path("", f)
+    return path("", f, name=name)
